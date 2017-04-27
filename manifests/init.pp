4f713846dae369c475cbc6142f6b9afdc1f363a1
Nilesh Barot <barotnilesh@Nileshs-MacBook-Pro.local>
2017-04-27 20:19:58 +0100

    Test the new module

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,12 @@
 class testmodule {
-
+  
+  if $::environment == 'development' {
   notice("The value is your $::environment")
+  }
+
+  if $facts[environment] == 'development' {
+  notice("Welcome to the $facts[environment]  $facts[role]")
+  }
+
 
 }
