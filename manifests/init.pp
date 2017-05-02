0d3ccd5a58972ad8442e78af9442f5b01c5370e6
Nilesh Barot <barotnilesh@Nileshs-MacBook-Pro.local>
2017-05-02 13:20:11 +0100

    update testmodule

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,19 +8,31 @@ class testmodule {
   Notify { "The provider is $provider - AZURE" : }
   }
 
-  if $::environment == 'development' {
-  Notify { "The environment is your $::environment" : }
+  if $environment == 'development' {
+  Notify { "The environment is $environment" : }
+  }
+
+  if $environment == 'preproduction' {
+  Notify { "The environment is $environment" : }
+  }
+
+  if $environment == 'production' {
+  Notify { "The environment is $environment" : }
   }
 
   if $role == 'web' {
-  Notify { "The role is (Webserver)  $role " : }
+  Notify { "The role is (Webserver) $role " : }
   }
 
   if $role == 'db' {
-  Notify { "The role is (Database)  $role " : }
+  Notify { "The role is (Database) $role " : }
   }
 
-  if $region == 'eu-west' {
+  if $region == 'euwest' {
+  Notify { "The server is in Region $region " : }
+  }
+
+  if $region == 'eucentral' {
   Notify { "The server is in Region $region " : }
   }
 
